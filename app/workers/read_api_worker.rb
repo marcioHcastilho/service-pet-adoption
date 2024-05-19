0bb816c41cc100b8d5b2c6ccd5a37cc74b0c751f
marcioHcastilho <120094486+Marciohenr1que@users.noreply.github.com>
2024-05-19 11:56:59 -0300

    update workes api

--- app/workers/read_api_worker.rb
+++ app/workers/read_api_worker.rb
@@ -2,21 +2,26 @@ class ReadApiWorker
   include Sidekiq::Worker
 
   def perform
-    breed_names = %w[caucasian_shepherd_dog bouvier_des_flandres grand_basset_griffon_vendeen]
-    breed_names.each do |breed_name|
-      breed_info = DogApiService.get_breed_info(breed_name)
+    response = DogApiService.fetch_breeds
+
+    return unless response.present? && response.code == 200
+
+    breeds = JSON.parse(response.body)['data']
+
+    breeds.each do |breed|
+      breed_info = DogApiService.parse_breed_info(breed['attributes'])
 
       next unless breed_info.present?
 
       BreedInfo.create!(
-        name: breed_name,
+        name: breed_info[:breed],
         description: breed_info[:description],
         life_span: breed_info[:life_span],
         male_weight: breed_info[:male_weight],
         female_weight: breed_info[:female_weight]
       )
     rescue StandardError => e
-      logger.error "Failed to process breed #{breed_name}: #{e.message}"
+      logger.error "Failed to process breed #{breed['attributes']['name']}: #{e.message}"
     end
   end
 end
